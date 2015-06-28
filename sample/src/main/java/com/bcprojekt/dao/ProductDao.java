5bb0f2fbb8abc317991d0177780aeb5c20a81328
Matt <Matt@192.168.1.101>
2015-06-28 13:08:57 +0200

    Productdao

---
+++ sample/src/main/java/com/bcprojekt/dao/ProductDao.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bcprojekt.dao;
+
+import com.bcprojekt.model.Product;
+import org.hibernate.type.IntegerType;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Matt
+ */
+public interface ProductDao extends CrudRepository <Product, IntegerType> {
+    
+}
