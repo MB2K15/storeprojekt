3823bff4feebb9a75d43e5ac7312a478b97dd947
pchlebda <piotrekchlebda@gmail.com>
2015-07-05 22:08:41 +0200

    Fix

--- src/main/java/com/bcprojekt/controller/CartController.java
+++ src/main/java/com/bcprojekt/controller/CartController.java
@@ -6,69 +6,59 @@
 package com.bcprojekt.controller;
 
 import com.bcprojekt.model.Product;
-import java.util.ArrayList;
-import java.util.List;
-import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- *
  * @author Matt
  */
 @Controller
 @RequestMapping(value = "/scart")
 public class CartController {
-    
+
     @RequestMapping(value = "/form")
-    public String index (ModelMap m ){
-        
+    public String index(ModelMap m) {
+
         m.put("product", new Product());
         return "form";
-        
+
     }
     //
-    
-    
-    @RequestMapping(value = "/addcart" , method = RequestMethod.POST)
-    public String addcart (@ModelAttribute("product")Product p , HttpSession session){
-        
-      List <Product> lista = (List <Product>)session.getAttribute("product");
-     
-     if (lista == null){
-      
-         lista = new ArrayList<>();
-         lista.add(p);
-         
-     }
-     
-    
 
-    
-    
-    session.setAttribute("product", lista);
-    session.setAttribute("wszystko", getAll(lista));
-    
-    return "product";
-    }
-    
-    public float getAll( List <Product> lista){
-        float wszystko = 0;
-        
-        for (Product product : lista) {
-            
-            wszystko += (product.getNumber() * product.getPrice());
-            
-            
+
+    @RequestMapping(value = "/addcart", method = RequestMethod.POST)
+    public String addcart(@ModelAttribute("product") Product p, HttpSession session) {
+        List<Product> lista = (List<Product>) session.getAttribute("product");
+
+        if (lista == null) {
+            lista = new ArrayList<>();
+            session.setAttribute("product",lista);
         }
-        
-        
-        
+        lista.add(p);
+        session.setAttribute("wszystko", getAll(lista));
+        return "product";
+    }
+
+    public float getAll(List<Product> lista) {
+        float wszystko = 0;
+
+        for (Product product : lista) {
+
+            wszystko += (product.getNumber() * product.getPrice());
+
+
+        }
+
+
         return wszystko;
     }
-    
-    
+
+
 }
--- src/main/webapp/WEB-INF/jsp/product.jsp
+++ src/main/webapp/WEB-INF/jsp/product.jsp
@@ -5,34 +5,34 @@
 --%>
 
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<%@page contentType="text/html" pageEncoding="windows-1250"%>
+<%@page contentType="text/html" pageEncoding="windows-1250" %>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=windows-1250">
-        <title>JSP Page</title>
-    </head>
-    <body>
-        <h1>Twój koszyk:</h1>
-        <table border = "1"  width="1000px" cellpadding="20px" style="border-collapse: collapse">
-            
-            <c:forEach items= '<%= request.getSession().getAttribute("product")%>' var="p">
-                
-                
-                <td>Nazwa : ${p.name}</td>
-                <td>Kategoria:${p.category}</td>
-                <td>Cena:${p.price}</td>
-                <td>Ilość:${p.number}</td>
-      
-            </c:forEach>
-                
-                <tr>
-                    <td colspan="5">
-                       Total:$ <%= request.getSession().getAttribute("wszystko")%>
-                        </td>
-                </tr>      
-        </table>
-                        
-                        <a href="./form.htm">Continue shopping....</a>
-    </body>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=windows-1250">
+    <title>JSP Page</title>
+</head>
+<body>
+<h1>Twój koszyk:</h1>
+<table border="1" width="1000px" cellpadding="20px" style="border-collapse: collapse">
+
+    <c:forEach items='<%= request.getSession().getAttribute("product")%>' var="p">
+
+        <tr>
+            <td>Nazwa : ${p.name}</td>
+            <td>Kategoria:${p.category}</td>
+            <td>Cena:${p.price}</td>
+            <td>Ilość:${p.number}</td>
+        </tr>
+    </c:forEach>
+
+    <tr>
+        <td colspan="5">
+            Total:$ <%= request.getSession().getAttribute("wszystko")%>
+        </td>
+    </tr>
+</table>
+
+<a href="./form.htm">Continue shopping....</a>
+</body>
 </html>
