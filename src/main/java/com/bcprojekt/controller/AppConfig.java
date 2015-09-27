45246505275b9c2b99f74b857580e1841f535bb2
admin <admin@INCOGNITO03>
2015-09-27 16:05:44 +0200

    Revert "Revert "Upload""
    This reverts commit 01b9e9f7aea274bc7f44474282f2f9d82362f184.

---
+++ src/main/java/com/bcprojekt/controller/AppConfig.java
@@ -0,0 +1,17 @@
+package com.bcprojekt.controller;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+        commonsMultipartResolver.setDefaultEncoding("utf-8");
+        commonsMultipartResolver.setMaxUploadSize(50000000);
+        return commonsMultipartResolver;
+    }
+}
